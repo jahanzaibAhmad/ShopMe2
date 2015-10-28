fe8f835260e7a0a44155ea5dbecfab246948c689
jahanzaibAhmad <jahanzaib.ahmad.76@gmail.com>
2015-10-28 11:45:01 +0500

    Initial

---
+++ ShopMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopMe.Startup))]
+namespace ShopMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
